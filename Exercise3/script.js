7c3f225878b31a3eac7ed58c0bbebbbe35c6c496
virak <virakson444@gmail.com>
2024-04-08 09:36:34 +0700

    update

--- Exercise3/script.js
+++ Exercise3/script.js
@@ -1,16 +1,20 @@
 //* 1
 
-// function square(num) {
-//   return num ** 2;
-// }
+function square(num) {
+  return num ** 2;
+}
 
-// function processArray(arr, callback) {
-//   return arr.map(callback);
-// }
+function processArray(arr, callback) {
+  const result = [];
+  for(let i = 0; i < arr.length; i++){
+    result.push(callback(arr[i]));
+  }
+  return result;
+}
 
-// const numbers = [1, 2, 3, 4, 5];
-// const squaredNumbers = processArray(numbers, square);
-// console.log(squaredNumbers);
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = processArray(numbers, square);
+console.log(squaredNumbers);
 
 //* 2
 
@@ -80,72 +84,72 @@
 
 //* 5
 
-const fs = require("fs");
+// const fs = require("fs");
 
-// Function to read file contents
-function readFile(filePath, callback) {
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      callback(err);
-      return;
-    }
-    callback(null, data);
-  });
-}
+// // Function to read file contents
+// function readFile(filePath, callback) {
+//   fs.readFile(filePath, "utf8", (err, data) => {
+//     if (err) {
+//       callback(err);
+//       return;
+//     }
+//     callback(null, data);
+//   });
+// }
 
-// Function to write file contents
-function writeFile(filePath, fileContents, callback) {
-  fs.writeFile(filePath, fileContents, (err) => {
-    if (err) {
-      callback(err);
-      return;
-    }
-    callback(null);
-  });
-}
+// // Function to write file contents
+// function writeFile(filePath, fileContents, callback) {
+//   fs.writeFile(filePath, fileContents, (err) => {
+//     if (err) {
+//       callback(err);
+//       return;
+//     }
+//     callback(null);
+//   });
+// }
 
-// Read input.txt, append "First modification" to the content, and write it to output1.txt
-readFile("input.txt", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
+// // Read input.txt, append "First modification" to the content, and write it to output1.txt
+// readFile("input.txt", (err, data) => {
+//   if (err) {
+//     console.error("Error reading file:", err);
+//     return;
+//   }
 
-  const modifiedContent = data + "First modification\n";
-  writeFile("output1.txt", modifiedContent, (err) => {
-    if (err) {
-      console.error("Error writing file:", err);
-      return;
-    }
+//   const modifiedContent = data + "First modification\n";
+//   writeFile("output1.txt", modifiedContent, (err) => {
+//     if (err) {
+//       console.error("Error writing file:", err);
+//       return;
+//     }
 
-    console.log("First modification done.");
+//     console.log("First modification done.");
 
-    // Read output1.txt, append "Second modification" to the content, and write it to output2.txt
-    readFile("output1.txt", (err, data) => {
-      if (err) {
-        console.error("Error reading file:", err);
-        return;
-      }
+//     // Read output1.txt, append "Second modification" to the content, and write it to output2.txt
+//     readFile("output1.txt", (err, data) => {
+//       if (err) {
+//         console.error("Error reading file:", err);
+//         return;
+//       }
 
-      const modifiedContent2 = data + "Second modification\n";
-      writeFile("output2.txt", modifiedContent2, (err) => {
-        if (err) {
-          console.error("Error writing file:", err);
-          return;
-        }
+//       const modifiedContent2 = data + "Second modification\n";
+//       writeFile("output2.txt", modifiedContent2, (err) => {
+//         if (err) {
+//           console.error("Error writing file:", err);
+//           return;
+//         }
 
-        console.log("Second modification done.");
+//         console.log("Second modification done.");
 
-        // Read output2.txt and print its content to the console
-        readFile("output2.txt", (err, data) => {
-          if (err) {
-            console.error("Error reading file:", err);
-            return;
-          }
+//         // Read output2.txt and print its content to the console
+//         readFile("output2.txt", (err, data) => {
+//           if (err) {
+//             console.error("Error reading file:", err);
+//             return;
+//           }
 
-          console.log("Content of output2.txt:\n", data);
-        });
-      });
-    });
-  });
-});
+//           console.log("Content of output2.txt:\n", data);
+//         });
+//       });
+//     });
+//   });
+// });
